63e806705cd99b5e8ddac25749cf16b46e010a28
jinaur <50763720+jinaur@users.noreply.github.com>
2023-06-24 17:12:19 +0900

    Initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-page class="column flex-center">
+    <q-knob
+      v-model="count"
+      :min="min"
+      :max="max"
+      size="80px"
+      show-value
+      :thickness="0.3"
+      color="primary"
+      track-color="dark"
+    >
+      <q-avatar size="75px">
+        <img alt="Quasar logo" src="~assets/quasar-logo-inner.svg" />
+      </q-avatar>
+    </q-knob>
+
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 140px"
+    />
+
+    <div class="q-mt-xl">
+      <q-btn
+        color="primary"
+        dense
+        round
+        label="-"
+        :disable="count === min"
+        @click="count--"
+      />
+
+      <span class="q-mx-md text-bold">{{ count }}</span>
+
+      <q-btn
+        color="primary"
+        dense
+        round
+        label="+"
+        :disable="count === max"
+        @click="count++"
+      />
+    </div>
+
+    <div class="q-mt-md" style="width: 200px">
+      <q-slider v-model="count" :min="min" :max="max" />
+    </div>
+
+    <div>
+      <q-btn label="1+1" />
+      <q-btn :label="now" />
+      <pre>now: {{ now }}</pre>
+    </div>
+    <div>
+      <q-input filled v-model="price" />
+      <pre>vat: {{ price * 0.1 }}</pre>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const min = -5;
+const max = 5;
+
+let now = ref(Date());
+let price = ref(100);
+let vat = ref();
+
+setInterval(() => {
+  now.value = Date();
+}, 100);
+</script>
